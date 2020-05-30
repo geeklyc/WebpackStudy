93c87e648dbafb083a730baef9c9984d2c734c3c
lyc <geeklyc@163.com>
2020-05-30 17:24:56 +0800

    [修改] 管理资源

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
+<!--
+ * @Description: 
+ * @Author: liyoucheng
+ * @Date: 2020-05-25 14:30:58
+ * @LastEditTime: 2020-05-30 17:24:20
+ * @LastEditors: liyoucheng
+--> 
 # WebpackStudy
 Webpack学习
+
+### 资源管理
+css
+```
+{
+  test: /\.css$/,
+  use: [
+    'style-loader',
+    'css-loader'
+  ]
+}
+```
+
+图片
+```
+{
+  test: /\.(png|svg|jpg|gif)$/,
+  use: [
+    'file-loader'
+  ]
+}
+```
+
+font（.woff格式可以）
+```
+{
+  test: /\.(woff|woff2|eot|ttf|otf)$/,
+  use: [
+    'file-loader'
+  ]
+}
+```
+
+文件
+要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader
+```
+{
+  test: /\.(csv|tsv)$/,
+  use: [
+    'csv-loader'
+  ]
+},
+{
+  test: /\.xml$/,
+  use: [
+    'xml-loader'
+  ]
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+/*
+ * @Description: 
+ * @Author: liyoucheng
+ * @Date: 2020-05-30 16:07:23
+ * @LastEditTime: 2020-05-30 17:17:58
+ * @LastEditors: liyoucheng
+ */ 
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'WEBPACK', '中文'], ' ');
   element.classList.add('hello');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+  console.log(Data);
+
+  // var myIcon = new Image();
+  // myIcon.src = Icon;
+  // element.appendChild(myIcon);
 
   return element;
 }
